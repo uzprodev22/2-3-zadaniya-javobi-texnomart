98df0e6e3dc877feb786d860066b3e726ee2f8e4
prodev22 <109823225+uzprodev22@users.noreply.github.com>
2023-01-02 13:06:04 +0500

    Add files via upload

---
+++ layouts/error.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="error404">
+        <div class="container text-center">
+           
+            <h1 class="error-404-title text-white" v-if="error.statusCode === 404">Afsuski bu sahifa mavjud emas!</h1>
+            <h1 class="error-404-title text-white" v-else>An error occurred</h1>
+            <div class="error-buttons">
+                <button @click="$router.go(-1)" class="btn btn-primary btn-hover-dark">
+                    <span class="button-text">Go Back Previous Page</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['error']
+    }
+</script>
+
+<style lang="scss" scoped>
+    .error404 {
+        display: flex;
+        height: 100vh;
+        align-items: center;
+        justify-content: center;
+        background-color: #111111;
+    }
+    .error-404-title {
+        font-weight: 500;
+        padding-top: 40px;
+        margin-bottom: 30px;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home-page-wrapper">
+        
+      
+        <ProductWrapper :isSectionTitle=true />
+        <BlogWrapper />
+        <TheFooter />
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+           
+            ServicePolicy: () => import('@/components/policy/ServicePolicy'),
+            ProductWrapper: () => import('@/components/product/ProductWrapper'),
+            BlogWrapper: () => import('@/components/BlogWrapper'),
+            TheFooter: () => import('@/components/TheFooter'),
+        },
+        head() {
+            return {
+                title: 'Home Fashion One'
+            }
+        },
+    };
+</script>
+
+
